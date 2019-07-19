9d5c273a2cf229c0bb2a51291c00f95fba997580
mayarunee <arunee@ampostech.com>
2019-07-19 11:40:48 +0700

    goal page

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,22 +4,35 @@ import Nav from 'react-bootstrap/Nav';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import Logo from '../../chef.png';
+import Logo2 from '../../intelligent.png';
+import Profile from '../../profile.png';
 
 class NavBar extends Component {
     render() {
         return (
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                </Nav>
-                <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search</Button>
-                </Form>
-            </Navbar>
+            <div className="nav-bar">
+                <div className="nav-content d-flex">
+                    <div className="logo">
+                        <img src={Logo}/>
+                        <img className="ml-3 mt-2" src={Logo2} width="111"/>
+                    </div>
+
+                    <div className="d-flex">
+                        <div className="search mr-5">
+                            <Form inline>
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                <Button variant="outline-info">Search</Button>
+                            </Form>
+                        </div>
+                        <div className="profile d-flex">
+                            <div className="text-right">Hi Jason Dunlop<br/><span className="text-secondary text-small">Genaral Manager</span></div>
+                            <div className="ml-3"><img className="profile-img" src={Profile}/></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         );
     }
 }
